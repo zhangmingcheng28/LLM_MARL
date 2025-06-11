f90826d706cb93dc9c973343e3e9d0a3d2b9a071
zhangmingcheng28 <zhangmingcheng28@hotmail.com>
2025-06-11 16:28:47 +0800

    Create pc_specific.py

---
+++ multi_agent_AAC_v2/pc_specific.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 11/6/2025 4:18 pm
+@Author  : Mingcheng
+@FileName: 
+@Description: 
+@Package dependency:
+"""
+
+
+class PCconfig:
+    def __init__(self):
+        self.save_file_prefix = 'D:\MADDPG_2nd_jp/'
+        self.shape_path = 'D:\deep_Q_learning\DQN_new_framework\lakesideMap\lakeSide.shp'
+        # self.
