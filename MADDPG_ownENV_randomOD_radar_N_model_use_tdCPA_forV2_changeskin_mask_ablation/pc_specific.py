079d70164170973697cf38c5b08ceb414d6cec6f
zhangmingcheng28 <zhangmingcheng28@hotmail.com>
2025-06-20 16:01:44 +0800

    mask_ablation_running

---
+++ MADDPG_ownENV_randomOD_radar_N_model_use_tdCPA_forV2_changeskin_mask_ablation/pc_specific.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 11/6/2025 4:18 pm
+@Author  : Mingcheng
+@FileName: 
+@Description: 
+@Package dependency:
+"""
+
+
+class PCconfig:
+    def __init__(self):
+        self.save_file_prefix = 'D:\MADDPG_2nd_jp/'
+        self.shape_path = 'D:\lakesideMap\lakeSide.shp'
+        self.head_not_inside_mask = r'F:\githubClone\LLM_MARL\multi_agent_AAC_v2\mask_model_out_mask_wTanh_v2.pth'
+        self.head_all_mask_head = r'F:\githubClone\LLM_MARL\multi_agent_AAC_v2\mask_model_fully_masked_wTanh_v2.pth'
+        self.head_inside_mask = r'F:\githubClone\LLM_MARL\multi_agent_AAC_v2\mask_model_eps20000_in_mask_wTanh_v2.pth'
+        self.svg_path = r'F:\githubClone\HotspotResolver_24\pictures\Aircraft.svg'
